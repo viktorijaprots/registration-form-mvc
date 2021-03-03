48f37cee06cb997dab18158a5be8275158408801
Viktorija <viktorija.protsenko@gmail.com>
2021-03-03 14:38:08 +0200

    Was implemented Singleton to connect to the database

--- src/application/core/DB.php
+++ src/application/core/DB.php
@@ -4,12 +4,18 @@ namespace application\core;
 
 class DB
 {
+	protected static $instance;
     public static function getConnect()
     {
-        // Getting configurations and connecting to the database
-        $params = include('config.php');
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $db = new \PDO($dsn, $params['user'], $params['password']);
-        return $db;
+		if(empty(self::$instance)) {
+			// Getting configurations and connecting to the database
+			$params = include('config.php');
+			$dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+			self::$instance = new \PDO($dsn, $params['user'], $params['password']);
+			self::$instance->query('SET NAMES utf8');
+			self::$instance->query('SET CHARACTER SET utf8');
+			
+		}
+		return self::$instance;
     }
 }
